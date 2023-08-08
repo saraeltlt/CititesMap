e43d867a403999b43f1d5529d175374a16724864
saraeltlt <saraeltlt@hotmail.com>
2023-08-09 00:05:57 +0300

    Initial Commit

---
+++ CititesMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CititesMap
+//
+//  Created by Sara Eltlt on 09/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
